9b84c985e6bfe12ff067d860f8498db886982068
Jori Regter <joriregter@gmail.com>
2017-09-05 11:13:40 +0200

    Add level 1

--- Assets/Scripts/ManagerScripts/GameManager.cs
+++ Assets/Scripts/ManagerScripts/GameManager.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
+	public float levelStartDelay = 2f;
+
 	void Start() {
 	}
 
     void Update()
     {
     }
+
+	public void StartLevel() {
+		SceneManager.LoadSceneAsync ("Level1");
+	}
+
+	void RestartLevel() {
+	}
+
+	void StopLevel() {
+	}
 }
