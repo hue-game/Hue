3e6376e99001aae7392d06dd4b4fce2882a9a8ee
luuktito <luuktito@gmail.com>
2017-09-07 13:33:21 +0200

    added EndBed script and canvas

---
+++ Assets/Scripts/ObjectScripts/Bed.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bed : MonoBehaviour {
+
+    public Image endFade;
+    private GameObject _player;
+    private GameManager _gameManager;
+
+    // Use this for initialization
+    void Start() {
+        _player = FindObjectOfType<Player>().gameObject;
+        _gameManager = FindObjectOfType<GameManager>();
+    }
+	
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == _player)
+        {
+            _player.GetComponent<Rigidbody2D>().simulated = false;
+            endFade.transform.parent.gameObject.SetActive(true);
+            StartCoroutine(EndLevel());
+        }
+    }
+
+    IEnumerator EndLevel()
+    {
+        float t = 0;
+
+        while (t < 1)
+        {
+            t += Time.deltaTime * (Time.timeScale / 4.0f);
+
+            endFade.color = Color.black * t;
+            yield return 0;
+        }
+        endFade.color = Color.black * 1.0f;
+
+        yield return new WaitForSeconds(1.0f);
+
+        _gameManager.StopLevel();
+
+    }
+
+}
