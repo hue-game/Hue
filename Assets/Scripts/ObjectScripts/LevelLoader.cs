eecddcdcb6de9cf382bf255391cb649b5ef5a03e
Jori Regter <joriregter@gmail.com>
2017-09-12 10:09:02 +0200

    Add level loader script

---
+++ Assets/Scripts/ObjectScripts/LevelLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour {
+    public GameManager gameManager;
+	public string levelToLoad;
+	public bool shouldInteract;
+
+	void LoadLevel(string levelToLoad) {
+		SceneManager.LoadSceneAsync (levelToLoad);
+	}
+	
+    void OnTriggerEnter2D(Collider2D other)
+    {
+		if (other.tag == "Player")
+		{
+			if (shouldInteract) {
+				
+			} else {
+				this.LoadLevel (levelToLoad);
+				// Load level
+			}
+        }
+    }
+}
