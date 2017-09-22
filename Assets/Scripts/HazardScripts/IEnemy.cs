4c7b99b09766839cc552a4dd1f9280ea8463e3ec
luuktito <luuktito@gmail.com>
2017-09-22 13:52:01 +0200

    small tweak

---
+++ Assets/Scripts/HazardScripts/IEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class IEnemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public abstract void Idle();
+
+    public abstract void Patrol();
+
+    public abstract void Attack();
+
+    public abstract void Lost();
+
+}
--- Assets/Scripts/ManagerScripts/ISpawnable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class ISpawnable : MonoBehaviour{
-
-    // Update is called once per frame
-    public abstract void Init();
-}
