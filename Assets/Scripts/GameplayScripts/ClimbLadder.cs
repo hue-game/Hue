2bd260f2fc0db7d43f5ef3766e3933e757681d69
Jori Regter <joriregter@gmail.com>
2017-09-25 09:37:28 +0200

    Ads level

---
+++ Assets/Scripts/GameplayScripts/ClimbLadder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbLadder : MonoBehaviour {
+	public float climbSpeed = 2.0f;
+	private float climbVelocity;
+	private Player _player;
+
+	// Use this for initialization
+	void Start () {
+		_player = FindObjectOfType<Player>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		InputManager inputManager = _player.GetComponent<InputManager> ();
+		Rigidbody2D rigidBody = _player.GetComponent<Rigidbody2D> ();
+
+		float moveY = inputManager.movementY;
+		float velocityY = moveY * climbSpeed;
+			
+		if (_player.onLadder && rigidBody != null) {
+			rigidBody.gravityScale = 0f;
+		}
+		else {
+			rigidBody.gravityScale = _player.InitialGravity;
+		}
+	}
+}
